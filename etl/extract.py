2945013f062b5e5df658c450a133fb8e1b2871a7
raeh16 <raeh160123@gmail.com>
2025-05-14 20:57:40 -0400

    function improvements

--- etl/extract.py
+++ etl/extract.py
@@ -1,4 +1,4 @@
-from exceptions import *
+import exceptions
 import requests
 from urllib.parse import urljoin
 
@@ -8,5 +8,5 @@ urlModifier = "events?active=true&closed=false&limit=500&sort=volume"
 def get_events():
     events = requests.get(urljoin(gammaAPI, urlModifier))
     if not events.ok:
-        raise EventsFetchException(f"Failed to fetch events: {events.status_code} - {events.text}")
+        raise exceptions.EventsFetchException(f"Failed to fetch events: {events.status_code} - {events.text}")
     return events.json()
